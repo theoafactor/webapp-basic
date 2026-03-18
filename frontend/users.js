e0394899b0beb3f43692ab36876c3a9b48d6637a
Olu Adeyemo <theoafactor@gmail.com>
2026-03-18 11:11:10 +0100

    Added user list

--- frontend/users.js
+++ frontend/users.js
@@ -5,5 +5,42 @@
 
     console.log(feedback.data)
 
+    let users = feedback.data;
+
+    // users data will be placed inside user-list class
+
+    // users.forEach(( user ) => {
+
+
+
+    // })
+    
+
+    for( let user in users){
+
+        let each_user = users[user];
+
+        let user_container = `
+              <div class="user-row">
+                <div class="flex-row">
+                    <div class="avatar-symbol">JD</div>
+                    <div class="user-info">
+                        <div class="user-name">
+                            ${each_user['fullname']}
+                            <span class="user-role">editor</span>
+                        </div>
+                        <div class="user-email">${each_user['email']}</div>
+                    </div>
+                </div>
+                <button class="delete-btn" type="button" onclick="alert('🗑️ delete user: Jessie Dele (mock) · no actual change')"><span>🗑️</span> delete</button>
+            </div>
+        `
+
+        document.querySelector(".user-list").innerHTML += user_container
+
+    }
+
+
+
 
 }())
